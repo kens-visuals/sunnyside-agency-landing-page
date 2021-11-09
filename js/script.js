b471cb23566b2d8512ae457f478d5401b01660e0
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-11-09 10:41:45 -0800

    fix a11y issues

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+const btn = document.querySelector('.js-btn');
 const list = document.querySelector('.js-list');
 const burger = document.querySelector('.js-burger');
 
-burger.addEventListener('click', () =>
-  list.classList.toggle('content__list--is-hidden')
-);
+burger.addEventListener('click', () => {
+  list.classList.toggle('content__list--is-hidden');
+  list.classList.contains('content__list--is-hidden')
+    ? btn.setAttribute('aria-expanded', 'false')
+    : btn.setAttribute('aria-expanded', 'true');
+});
